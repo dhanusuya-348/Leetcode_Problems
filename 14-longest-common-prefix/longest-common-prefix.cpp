3d2e93e1c1bc926b60aa7daa295073dede145e27
DHANUSUYA R <129606380+dhanusuya-348@users.noreply.github.com>
2024-09-21 21:53:01 +0530

    Time: 6 ms (32.29%) | Memory: 11.1 MB (76.11%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.cpp
+++ 14-longest-common-prefix/longest-common-prefix.cpp
@@ -6,29 +6,25 @@ public:
             return "";
         }
         
-        // Find the string with the minimum length
         string minstr = strs[0];
-        for(int i=1; i<strs.size(); i++) {  // start from i=1 as minstr is initialized with strs[0]
+        for(int i=1; i<strs.size(); i++) {  
             if(strs[i].length() < minstr.length()) {
                 minstr = strs[i];
             }
         }
         
-        if(minstr == "") {  // If the shortest string is empty, return empty
-            return "";
+        if(minstr == "") {  
         }
         
         int minlen = minstr.length();
-
-        // Compare all strings with the minimum string character by character
         for(int j=0; j<minlen; j++) {  
-            for(int i=0; i<strs.size(); i++) {  // Compare the same index across all strings
-                if(strs[i][j] != minstr[j]) {  // If there's a mismatch
-                    return minstr.substr(0, j);  // Return the common prefix found so far
+            for(int i=0; i<strs.size(); i++) {  
+                if(strs[i][j] != minstr[j]) {  
+                    return minstr.substr(0, j);  
                 }
             }
         }
 
-        return minstr;  // If all characters match for the entire length of minstr
+        return minstr;  
     }
 };
