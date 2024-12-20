e537dd74d6d9b85cef4801c80eaa9529a4edacba
DHANUSUYA R <129606380+dhanusuya-348@users.noreply.github.com>
2024-12-20 10:03:11 +0530

    Time: 3 ms (19.35%) | Memory: 8.6 MB (18.98%) - LeetSync

---
+++ 69-sqrtx/sqrtx.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int mySqrt(int x) {
+        int low=1;
+        int high=x;
+        long mid=0;
+        while(low<=high){
+            mid = low+((high-low)/2);
+            if((mid*mid)<x){
+                low=mid+1;
+            }
+            else if((mid*mid)>x){
+                high=mid-1;
+            }
+            else{
+                return mid;
+            }
+        }
+        return high;
+    }
+};
